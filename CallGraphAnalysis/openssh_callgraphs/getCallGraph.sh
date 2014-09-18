7ecdd1f56bf09e7b27b7fbf22fdfe1a09db25f23
Kaeser <kmsabrin@gmail.com>
2014-09-18 16:13:05 -0400

    removing data

--- CallGraphAnalysis/openssh_callgraphs/getCallGraph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for ((i = 1; i < 40; i++))
-do
-	gcc  "openssh-$i.c" -o "openssh-$i.o" -lssl -lcrypt -lresolv -lutil -lpam
-	genfull -g cobjdump -o "full.graph-openssh-$i"
-	rm -f "openssh-$i.o"
-done
